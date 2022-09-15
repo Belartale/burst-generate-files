a733585b8f065c1ce76b7d629380a91282e5bec6
Belartale <belartale@gmail.com>
2022-09-15 14:24:26 +0300

    remove coment

--- src/utils/getSelectedName.ts
+++ src/utils/getSelectedName.ts
@@ -38,27 +38,5 @@ export const getSelectedName = async (strings: string | string[]): Promise<types
         result = await getName({ replaceVar: strings, result });
     }
 
-    // for await (const string of strings) {
-    //     const replacerQuestion: any = {
-    //         type:     'input',
-    //         name:     'selectedName',
-    //         message:  `Replace ${string} on >>> `,
-    //         validate: (value: string) => {
-    //             const isValid: boolean = Boolean(value.trim());
-
-    //             return isValid || 'You must provide an answer!!!';
-    //         },
-    //     };
-
-    //     const gotValue: {selectedName: string} = await enquirer.prompt(replacerQuestion);
-
-    //     result = [
-    //         ...result, {
-    //             replaceVar: string,
-    //             value:      gotValue.selectedName.trim(),
-    //         },
-    //     ];
-    // }
-
     return result;
 };
