025bb58b041f2d240caf0cd213d151ddea7f8faf
Belartale <belartale@gmail.com>
2022-09-24 12:39:01 +0300

    add new cases

--- src/utils/replaceWordCase/index.ts
+++ src/utils/replaceWordCase/index.ts
@@ -27,7 +27,56 @@ const cases = ({ stringReplace, result }: types.Cases) => {
 
     if (result && result.includes(`${stringReplace.replaceVar}(kebabCase)`)) { // lorem lorem => lorem-lorem
         const re = new RegExp(`${stringReplace.replaceVar}.kebabCase.`, 'g');
-        const modifiedToKebabCase = newString.map((word) => word.toLowerCase()).join('-');
+        const modifiedToKebabCase = newString.map((word) => word).join('-');
+
+        newResult = newResult.replace(re, modifiedToKebabCase);
+    }
+
+    if (result && result.includes(`${stringReplace.replaceVar}(dotCase)`)) { // lorem lorem => lorem.lorem
+        const re = new RegExp(`${stringReplace.replaceVar}.dotCase.`, 'g');
+        const modifiedToKebabCase = newString.map((word) => word).join('.');
+
+        newResult = newResult.replace(re, modifiedToKebabCase);
+    }
+
+    if (result && result.includes(`${stringReplace.replaceVar}(lowerCase)`)) { // LOREM LoRem => loremlorem
+        const re = new RegExp(`${stringReplace.replaceVar}.lowerCase.`, 'g');
+        const modifiedToKebabCase = newString.map((word) => word.toLowerCase()).join('');
+
+        newResult = newResult.replace(re, modifiedToKebabCase);
+    }
+
+    if (result && result.includes(`${stringReplace.replaceVar}(pathCase)`)) { // lorem lorem => lorem/lorem
+        const re = new RegExp(`${stringReplace.replaceVar}.pathCase.`, 'g');
+        const modifiedToKebabCase = newString.map((word) => word).join('/');
+
+        newResult = newResult.replace(re, modifiedToKebabCase);
+    }
+
+    if (result && result.includes(`${stringReplace.replaceVar}(sentenceCase)`)) { // lorem lorem => Lorem lorem
+        const re = new RegExp(`${stringReplace.replaceVar}.sentenceCase.`, 'g');
+        const modifiedToKebabCase = newString.map((word, index) => {
+            if (index === 0) {
+                return word.split('').map((letter, index) => index === 0 ? letter.toUpperCase() : letter)
+                    .join('');
+            }
+
+            return word;
+        }).join(' ');
+
+        newResult = newResult.replace(re, modifiedToKebabCase);
+    }
+
+    if (result && result.includes(`${stringReplace.replaceVar}(snakeCase)`)) { // lorem lorem => lorem_lorem
+        const re = new RegExp(`${stringReplace.replaceVar}.snakeCase.`, 'g');
+        const modifiedToKebabCase = newString.map((word) => word).join('_');
+
+        newResult = newResult.replace(re, modifiedToKebabCase);
+    }
+
+    if (result && result.includes(`${stringReplace.replaceVar}(titleCase)`)) { // lorem lorem => Lorem Lorem
+        const re = new RegExp(`${stringReplace.replaceVar}.titleCase.`, 'g');
+        const modifiedToKebabCase = newString.map((word) => word[ 0 ].toUpperCase() + word.slice(1)).join(' ');
 
         newResult = newResult.replace(re, modifiedToKebabCase);
     }
