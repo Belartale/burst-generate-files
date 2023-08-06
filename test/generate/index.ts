0c09761975825829dd1fb40a9d8d0bded993bb48
Belartale <belartale@gmail.com>
2023-08-06 16:06:29 +0300

    add folder for testing and finish pre setting

---
+++ test/generate/index.ts
@@ -0,0 +1,49 @@
+// Core
+import chalk from 'chalk';
+
+// Constants
+import { MODE } from '../../scripts/webpack/constants';
+
+// Core
+const PACKAGE = import(process.env.TEST_MODE === MODE.PRODUCTION ? '../../dist' : '../../src');
+
+const folderForTesting = 'test'; // if folder changed, change it in package.json too (npm scripts) and in tsconfig.generate.json
+const pathToTemplate = `./${folderForTesting}/generate/templates`;
+
+PACKAGE.then(({ CLIGen }) => {
+    console.log(chalk.yellow(`Test started with mode: ${process.env.TEST_MODE || MODE.DEVELOPMENT}\n`));
+
+    CLIGen([
+        {
+            name:      'Component: ./src/view/components/__componentName__',
+            templates: [
+                {
+                    stringsReplacers: '__componentName__',
+                    pathToTemplate:   `${pathToTemplate}/component`,
+                    outputPath:       `./${folderForTesting}/src/components/__componentName__(pascalCase)`,
+                    markers:          [
+                        {
+                            pattern:        '// MarkerGen re-export',
+                            markerTemplate: `${pathToTemplate}/component/.genignore/export.ts`,
+                            pathToMarker:   `./${folderForTesting}/src/components/index.ts`,
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            name:      'selectDirectory Component: ./src/view/components/__componentName__',
+            templates: [
+                {
+                    stringsReplacers: '__componentName__',
+                    pathToTemplate:   `${pathToTemplate}/component`,
+                    outputPath:       `./${folderForTesting}/src/components/__componentName__(pascalCase)`,
+                    selectDirectory:  true,
+                },
+            ],
+        },
+    ], {
+        showFullError: true,
+    });
+});
+
